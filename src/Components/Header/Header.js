94d1b7e8e69334861f3280b741e1265b2e320687
Rince Mathew <rincemathewmanattu@gmail.com>
2022-10-06 23:22:44 +0530

    header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import './Header.css'
+import {Link} from 'react-router-dom'
 
 function Header() {
   return (
     <div>
-        <div className='div' style={{height:'96px'}}></div>
+      <div className="head">
+        <div className="percentage head-item">
+          <img src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png" alt='img'></img>
+          <div><Link className='head-button' to="/mobiles">Go to mobiles</Link></div>
+          <div><Link className='head-button' to="/audio">Go to Audio(headphone)</Link></div>
+        </div>
+      </div>
+      <div style={{height:'56px'}}></div>
+      <div className="nav">
+        <div className="percentage nav-item">
+          <div className="nav-button">Electronics</div>
+          <div className="nav-button">TVs & Appliances</div>
+          <div className="nav-button">Men</div>
+          <div className="nav-button">Women</div>
+          <div className="nav-button">Baby & Kids</div>
+          <div className="nav-button">Home & Furniture</div>
+          <div className="nav-button">Sports, Books & More</div>
+          <div className="nav-button">Flights</div>
+          <div className="nav-button">Offer Zone</div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
   <React.StrictMode>
-    <Header/>
     <BrowserRouter>
+    <Header/>
     <Routes>
-    <Route path='mobiles' element={<App title='mobile' />}/>
-    <Route path='audio' element={<App title='audio' />}/>
+    <Route path='mobiles' element={<App title='mobile'/>}/>
+    <Route path='audio' element={<App title='audio'/>}/>
     <Route path="*"
       element={
         <main style={{ padding: "1rem" }}>
