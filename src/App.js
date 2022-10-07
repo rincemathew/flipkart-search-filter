1b69364decc64c55e0db069f39df22805507d3c7
Rince Mathew <rincemathewmanattu@gmail.com>
2022-10-07 17:45:27 +0530

    data loaded

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import {mobile, audio} from './Constants'
+import axios from 'axios';
 
-function App() {
+function App(props) {
+var item = props.title === mobile.name ? mobile:audio;
+const [rsData, setRsData] = useState([])
+useEffect(() => {
+  axios.get(item.url).then((resp)=>{
+    // console.log(resp.data.results)
+    setRsData(resp.data.results)
+  })
+
+}, [item])
+
+
   return (
-    <div className="App">
-      <div>hello</div>
+    <div className="app">
+      <div className='filter'>
+        {props.title}
+        {item.url}
+      </div>
+      <div className='list-item'>
+        {rsData.map((obj => <div>
+          {obj.mobile_variants}
+          {/* {console.log(`${obj}.${item.modelName}`)} */}
+        {console.log(obj[item.modelName])}
+        </div>))}
+      </div>
     </div>
   );
 }
---
+++ src/Constants.js
@@ -0,0 +1,13 @@
+export const mobile = {name:'mobile',
+    url:'https://www.gadget4india.com/api/mobilelist',
+    modelName:'mobileNames.mobile_name',
+    brandName:'mobileNames.brandName.brand_name',
+}
+
+
+export const audio = {name:'audio',
+    url:'https://www.gadget4india.com/earwear/api/earwearlist',
+    modelName:'ear_name',
+    brandName:'earBrandName.ear_brand_name',
+}
+
