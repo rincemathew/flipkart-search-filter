d8bd944325ebf1ed426a63f5eaad1f363ec5c04e
Rince Mathew <rincemathewmanattu@gmail.com>
2022-10-08 20:12:33 +0530

    item design completed

--- src/App.js
+++ src/App.js
@@ -35,8 +35,21 @@ useEffect(() => {
         {rsData.map((obj => <div>
           <div className='list-obj'>
           <div className='list-obj-1'><img src={item==mobile?obj.mobileNames?.mobile_image:obj.ear_picture} alt="" /></div>
-          <div className='list-obj-2'>jkjjghg</div>
-          <div className='list-obj-3'>gughhj</div>
+          <div className='list-obj-2'>
+            {item==mobile?<div className='list-obj-2-div'>{obj?.mobileNames?.brandName.brand_name} {obj?.mobileNames?.mobile_name} {`(${obj.mobileStorage?.device_storage} GB)`}</div>:<div className='list-obj-2-div'>{obj.earBrandName?.ear_brand_name} {obj.ear_name}</div>}
+            <ul className='list-obj-2-ul'>
+              {item==mobile?<li>{`${obj.mobilePerformance?.ram} GB RAM | ${obj.mobileStorage?.device_storage} GB ROM`}</li>:<li>{obj.ear_type}</li>}
+              {item==mobile?<li>{`${obj.mobileDisplay?.screen_size} inch ${obj.mobileDisplay?.display_type} GB ROM`}</li>:<li>{`WaterProof:${obj.ear_waterproof}`}</li>}
+              {item==mobile?<li>{`${obj.mobileCamera?.primary_camera_str} | ${obj.mobileCamera?.secondary_camera_str}`}</li>:''}
+              {item==mobile?<li>{`${obj.mobileBattery?.battery_capacity} mAh ${obj.mobileBattery?.battery_type} Battery`}</li>:''}
+              {item==mobile?<li>{`${obj.mobilePerformance?.processor_company} ${obj.mobilePerformance?.processor}`}</li>:''}
+              {item==mobile?<li>{obj.otherFeature?.warranty}</li>:''}
+            </ul>
+          </div>
+          <div className='list-obj-3'><div className='obj-3-price'>₹{item==mobile?obj.mobileGeneral?.price:obj.ear_price}<span><img src='https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/fa_62673a.png'></img></span></div>
+          <p>Free delivery</p>
+          <p>Top Discount on Sale</p>
+          </div>
           </div>
           <hr></hr>
         </div>))}
--- src/Components/Listlist/ListItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ListItem() {
-  return (
-    <div>ListItem</div>
-  )
-}
-
-export default ListItem
